00eddacc431de249322088d8e1320d5e702d0441
cyanjade <cyanjd@stanford.edu>
2024-05-29 14:37:45 -0700

    added initial set of dialogue for npc character

---
+++ characters/character_scenes/inside_truck_placeholder_character.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+var is_roaming = true
+var is_chatting = false
+var player_in_area = false
+
+func _process(delta):
+	if player_in_area:
+		if Input.is_action_just_pressed("e"):
+			#run dialogue
+				run_dialogue("siblingChickGiving")
+				
+func run_dialogue(dialogue_string):
+	is_chatting = true
+	is_roaming = false
+	
+	Dialogic.start(dialogue_string)
+
+func _on_chat_detection_body_entered(body):
+	if body.has_method("player"):
+		player_in_area = true
+	pass # Replace with function body.
+
+
+func _on_chat_detection_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	if body.has_method("player"):
+		player_in_area = false
+	pass # Replace with function body.
--- characters/character_scenes/placeholder_character.gd
+++ characters/character_scenes/placeholder_character.gd
@@ -1,53 +1,68 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
 @onready var sprite_2d = $Sprite2D
+
+@export var move_speed : float = 300
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var isDragging = false;
-var pushforce = 80;
-var inertia = 100;
-
-#func _input(event):
-	#if event is InputEventMouseButton:
-		#if event.is_pressed() and event.button_index == BUTTON_LEFT:
-			#is_dragging = true
-		#else:
-			#is_dragging = false
 
 func _physics_process(delta):
+	# Get input direction
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+
 	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "walkRight"
+		if velocity.x < -1:
+			sprite_2d.flip_h = true
+		elif velocity.x > 1:
+			sprite_2d.flip_h = false
+	elif (velocity.y > 1 || velocity.y < -1):
 		sprite_2d.animation = "walkRight"
 	else:
 		sprite_2d.animation = "defaultBob"
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Update velocity
+	velocity = input_direction * move_speed
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# Move character on map
 	move_and_slide()
-	#for i in get_slide_collision_count():
-		#var collision = get_slide_collision(i)
-		#if collision.get_collider.is_in_group("movable-object"):
-			#collision.collider.apply_central_impulse(-collision.normal + inertia)
-		#
+
+
+
+#func _physics_process(delta):
+	#if (velocity.x > 1 || velocity.x < -1):
+		#sprite_2d.animation = "walkRight"
+	#else:
+		#sprite_2d.animation = "defaultBob"
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity.y += gravity * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
+	#
+	#var isLeft = velocity.x < 0
+	#sprite_2d.flip_h = isLeft
 	
-	var isLeft = velocity.x < 0
-	sprite_2d.flip_h = isLeft
+func player():
+	pass
 	
 	#if is_dragging:
 		#var input_vec = Input.get_action_strength("ui_mouse") * Vector2(1, -1)
