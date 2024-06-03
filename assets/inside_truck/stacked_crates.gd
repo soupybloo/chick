e9203e8f63b9831e3f9e26628f9aff48840c0f02
cyanjade <cyanjd@stanford.edu>
2024-06-03 16:08:00 -0700

    Added crate to inside truck scene

---
+++ assets/inside_truck/stacked_crates.gd
@@ -0,0 +1,50 @@
+extends Area2D
+
+var player
+var player_in_chat_zone = false
+
+var can_start_dialogue = true
+var dialogue_timeout = 0.5 # Half a second delay
+
+const boundary_size = 20
+
+func _ready():
+	randomize()
+	Dialogic.signal_event.connect(DialogicSignal)
+	print("Dialogic signal connected")
+
+func _process(delta):
+	if player_in_chat_zone:
+		if Input.is_action_just_pressed("e") and can_start_dialogue:
+			print("E pressed, starting dialogue")
+			_start_dialogue("stacked_crates_popup")
+
+func _start_dialogue(dialogue_string) -> void:
+	var dialog = Dialogic.start(dialogue_string)
+	add_child(dialog)
+	dialog.visible = true
+	Dialogic.timeline_ended.connect(ended_dialogue)
+	can_start_dialogue = false
+	await get_tree().create_timer(dialogue_timeout).timeout
+	can_start_dialogue = true
+
+func ended_dialogue() -> void:
+	Dialogic.timeline_ended.disconnect(ended_dialogue)
+	print("Dialogue ended")
+
+func DialogicSignal(arg: String) -> void:
+	if arg == "exit_chick":
+		print("signal received")
+
+func choose(array) -> Variant:
+	array.shuffle()
+	return array.front()
+
+func _on_chat_detection_body_entered(body):
+	if body.has_method("player"):
+		player = body
+		player_in_chat_zone = true
+
+func _on_chat_detection_body_exited(body):
+	if body.has_method("player"):
+		player_in_chat_zone = false
