3d427f00918f4e2d2eef549f1d02a5b44a357e5f
cyanjade <cyanjd@stanford.edu>
2024-06-07 19:55:53 -0700

    Added highlights to flowers

--- scenes/Flower.gd
+++ scenes/Flower.gd
@@ -1,11 +1,24 @@
 extends Area2D
 
+var material_set = false
+var _material = null
+
+func _process(delta):
+	if $Sprite2D.material != null and material_set == false and _material == null:
+		material_set = true
+		_material = $Sprite2D.material 
+		_material.set_shader_parameter("line_color", Color(1.0, 1.0, 1.0, 0.0))
+
 func _on_body_entered(body):
 	if body.name == "Player":
 		body.nearby_flower = self
-
+		if _material != null:
+			_material.set_shader_parameter("line_color", Color(1.0, 1.0, 0.0, 1.0))
 
 func _on_body_exited(body):
+	if body.name == "Player":
+		if _material != null:
+			_material.set_shader_parameter("line_color", Color(1.0, 1.0, 1.0, 0.0))
 	if body.is_in_group("Player"):
 		body.nearby_flower = null
-
+		
